f2c797162fedb8b2c8a7195c587db187c31b06d5
OHart95 <111344033+OHart95@users.noreply.github.com>
2025-11-12 15:37:04 +0000

    update config files ready for deployment

--- react/vite.config.js
+++ react/vite.config.js
@@ -2,8 +2,11 @@ import { defineConfig } from 'vite'
 import path from 'path';
 import react from '@vitejs/plugin-react'
 
+const repoName = 'my-fullstack-portfolio'
+
 // https://vite.dev/config/
 export default defineConfig({
+  base: `/${repoName}/`,
   plugins: [react()],
   resolve: {
     alias: {
